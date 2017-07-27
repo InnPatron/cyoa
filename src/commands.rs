99abacae57c88ae72f5393b65c9de15302e0a210
Random_Civvy <vomalexander@gmail.com>
2017-07-27 11:07:10 -0700

    Add GameState command. if args.len() == 0, return the current game state if args.len() == 1, set the game state to a NUMBER

--- src/commands.rs
+++ src/commands.rs
@@ -15,3 +15,22 @@ impl Cmd for Display {
         Ok(ExecSignal::NextInstruction(None))
     }
 }
+
+#[derive(Clone, Debug)]
+pub struct GameState(pub RcValue);
+
+impl Cmd for GameState {
+    fn execute(&self, stack: &mut Stack, args: Vec<CIR>) -> Result<ExecSignal, ExecErr> {
+        max_args!(args, 1);
+        if args.len() == 0 {
+            return Ok(ExecSignal::NextInstruction(Some(self.0.clone())));
+        } else {
+            {
+                cir_extract!(args[0] => Number)?;
+            }
+            *self.0.borrow_mut() = args[0].value.inner_clone();
+        }
+        
+        Ok(ExecSignal::NextInstruction(None))
+    }
+}
