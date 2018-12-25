f211a964b67def3f1ac9b4af519cbe6c23a137b5
Alexander Vo <randomcivvy0121@gmail.com>
2018-12-24 20:56:06 -0700

    Set up main to play the story

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,36 @@ mod display;
 mod game_screen;
 mod title_screen;
 
+use failure::Error;
+
 use crate::game::GameInstance;
 
 fn main() {
     let library = library::scan_library();
     let story = title_screen::title_screen(&library);
+
+    match run(story) {
+        Ok(_) => (),
+
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    }
+}
+
+fn run(story: Option<&library::StoryHandle>) -> Result<(), Error> {
+    match story {
+        Some(story) => {
+            let game_instance = game::GameInstance::new(story)?;
+
+            game_screen::game_screen(game_instance)?;
+
+            Ok(())
+        }
+
+        None => {
+            Ok(())
+        }
+    }
 }
