a08bbe5ac07491748983ca7b6dd6a2f265906bda
Alexander Vo <randomcivvy0121@gmail.com>
2018-12-24 21:14:32 -0700

    Update the game screen to allow user quitting.

--- src/game_screen.rs
+++ src/game_screen.rs
@@ -9,12 +9,23 @@ use crate::script_lib;
 use crate::display;
 use crate::input::*;
 
+enum Decision {
+    Continue,
+    Quit,
+}
+
 pub fn game_screen(mut game_instance: GameInstance) -> Result<(), Error> {
     loop {
         let game_state = game_instance.context().state();
 
         match game_state {
-            script_lib::STATE_RUN => game_iteration(&mut game_instance)?,
+            script_lib::STATE_RUN => {
+                match game_iteration(&mut game_instance)? {
+                    Decision::Continue => (),
+                    
+                    Decision::Quit => return Ok(()),
+                }
+            }
 
             script_lib::STATE_END => break,
 
@@ -25,7 +36,7 @@ pub fn game_screen(mut game_instance: GameInstance) -> Result<(), Error> {
     Ok(())
 }
 
-fn game_iteration(game_instance: &mut GameInstance) -> Result<(), Error> {
+fn game_iteration(game_instance: &mut GameInstance) -> Result<Decision, Error> {
     let choices = game_instance.context().choices();
 
     display::list(choices
@@ -33,12 +44,14 @@ fn game_iteration(game_instance: &mut GameInstance) -> Result<(), Error> {
                   .map(|c| c.display())
                   .enumerate());
 
-    prompt();
     let choice = loop {
+
+        prompt();
         match number() {
 
             InputResult::Quit => {
                 println!("Exiting...\n");
+                return Ok(Decision::Quit);
             }
 
             InputResult::Invalid(String) => {
@@ -55,7 +68,9 @@ fn game_iteration(game_instance: &mut GameInstance) -> Result<(), Error> {
         }
     };
 
-    game_instance.execute_choice(choice)
+    game_instance.execute_choice(choice)?;
+
+    return Ok(Decision::Continue);
 }
 
 fn prompt() {
